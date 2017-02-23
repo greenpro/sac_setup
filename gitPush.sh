03b7bfa9e07112445e6c8f9ec8a14ad03d3910bf
Craig Christensen <crg.chrstnsn@gmail.com>
2017-02-23 10:38:35 -0600

    Changes to the push to allow one comment for all

--- gitPush.sh
+++ gitPush.sh
@@ -1,48 +1,50 @@
-COMMENT="Changes to the launch files to allow moveit to run."
+echo "Enter a comment: "
+read COMMENT
+echo "--$COMMENT--"
 
 cd src/sac_config
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../sac_controllers
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../sac_description
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../sac_drivers
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../sac_gazebo
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../sac_translators
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../sac_launch
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../scorbot_config
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../scorbot_config
 git add .
-git commit -m $(COMMENT)
+git commit -m "$COMMENT"
 git push
 
 cd ../..
