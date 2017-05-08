93cceab00ab09e4be5d3a31d2d76891f04b4f5fe
Craig Christensen <crg.chrstnsn@gmail.com>
2017-05-08 11:36:18 -0400

    Changes to the installation

--- setup.sh
+++ setup.sh
@@ -2,18 +2,18 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install -y ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 # Install ROS packages
-sudo apt-get install -y python-rosinstall 
-sudo apt-get install -y ros-kinetic-gazebo8-ros-pkgs 
-sudo apt-get install -y ros-kinetic-gazebo8-ros-control 
-sudo apt-get install -y ros-kinetic-ros-control 
-sudo apt-get install -y ros-kinetic-ros-controllers 
+sudo apt-get install python-rosinstall 
+sudo apt-get install ros-kinetic-gazebo8-ros-pkgs 
+sudo apt-get install ros-kinetic-gazebo8-ros-control 
+sudo apt-get install ros-kinetic-ros-control 
+sudo apt-get install ros-kinetic-ros-controllers 
 #sudo apt-get install -y ros-kinetic-moveit
 
 # Install Gazebo
